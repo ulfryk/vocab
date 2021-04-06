a193eaeadc62b36769b8143be6c1244e139322e6
Jakub Strojewski <jakub.strojewski@openpayd.com>
2021-04-06 14:56:47 +0100

    refactor

--- src/Main.elm
+++ src/Main.elm
@@ -8,15 +8,13 @@ import Layout exposing (layout)
 import List
 
 import Manage exposing (ManageMsg)
-import Play exposing (PlayMsg(..))
+import Play exposing (PlayMsg(..), updateOnPlay)
 import Random exposing (Generator)
 import Set exposing (insert, union)
 import State exposing (Card, Model, Scope(..), Showing(..), getNthCard, getPlayableCards, initial, initialGameStats, rollRandomCardIndex)
 
 type Msg = Play PlayMsg | Manage ManageMsg
 
-playNthCard : Int -> Msg
-playNthCard n = Play <| SetNth n
 
 initialModel : () -> (Model, Cmd Msg)
 initialModel _ = (initial, Cmd.none)
@@ -24,32 +22,16 @@ initialModel _ = (initial, Cmd.none)
 subscriptions : Model ->  Sub Msg
 subscriptions _ = Sub.none
 
+mapSubUpdateCmd : (a -> Msg) -> (Model, Cmd a) -> (Model, Cmd Msg)
+mapSubUpdateCmd mapper (m, c) = (m, Cmd.map mapper c)
+
 main =
   Browser.element { init = initialModel, update = update, view = view, subscriptions = subscriptions }
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg ({ game, archived } as model) =
   case msg of
-    Play m ->
-        case m of
-            Start -> ( model, Random.generate playNthCard (rollRandomCardIndex model) )
-            End -> ({ model | scope = Splash, game = initialGameStats, archived = union archived game.perfect }, Cmd.none)
-            Next id ->
-                let ml = { model | game = { game | good = insert id game.good } } in
-                (ml, Random.generate playNthCard <| rollRandomCardIndex ml)
-            Drop id ->
-                let ml = { model | game = { game | perfect = insert id game.perfect } } in
-                (ml, Random.generate playNthCard <| rollRandomCardIndex ml)
-            Fail id ->
-                let ml = { model | game = { game | bad = insert id game.bad } } in
-                (ml, Random.generate playNthCard <| rollRandomCardIndex ml)
-            Show card -> ({ model | scope = Playing Both card }, Cmd.none)
-            SetNth n ->
-                case (getNthCard n model) of
-                    Just card -> ({ model |scope = Playing ASide card }, Cmd.none)
-                    Nothing -> ({ model | scope = Done }, Cmd.none)
-
-
+    Play m -> mapSubUpdateCmd Play <| updateOnPlay m model
     Manage m -> (model, Cmd.none)
 
 view : Model -> Html Msg
--- src/Play.elm
+++ src/Play.elm
@@ -1,5 +1,28 @@
 module Play exposing (..)
 
-import State exposing (Card)
+import Random exposing (Generator)
+import Set exposing (insert, union)
+
+import State exposing (Card, GameStats, Model, Scope(..), Showing(..), getNthCard, initialGameStats, rollRandomCardIndex)
+
 
 type PlayMsg = Start | Show Card | Fail String | Next String | Drop String | SetNth Int | End
+
+updateGameAndRollNextIndex : Model -> GameStats -> (Model, Cmd PlayMsg)
+updateGameAndRollNextIndex model game =
+    let ml = { model | game = game } in
+    (ml, Random.generate SetNth <| rollRandomCardIndex ml)
+
+updateOnPlay : PlayMsg -> Model -> (Model, Cmd PlayMsg)
+updateOnPlay m ({ archived, game } as model) =
+        case m of
+            Start -> ( model, Random.generate SetNth (rollRandomCardIndex model) )
+            End -> ({ model | scope = Splash, game = initialGameStats, archived = union archived game.perfect }, Cmd.none)
+            Next id -> updateGameAndRollNextIndex model { game | good = insert id game.good }
+            Drop id -> updateGameAndRollNextIndex model { game | perfect = insert id game.perfect }
+            Fail id -> updateGameAndRollNextIndex model { game | bad = insert id game.bad }
+            Show card -> ({ model | scope = Playing Both card }, Cmd.none)
+            SetNth n ->
+                case (getNthCard n model) of
+                    Just card -> ({ model |scope = Playing ASide card }, Cmd.none)
+                    Nothing -> ({ model | scope = Done }, Cmd.none)
