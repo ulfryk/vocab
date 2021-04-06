a432eb4cca2c6aa62a1c1288a146ebeff0e9b36e
Jakub Strojewski <jakub.strojewski@openpayd.com>
2021-04-06 22:08:21 +0100

    more styles

--- src/DoneHtml.elm
+++ src/DoneHtml.elm
@@ -1,12 +1,20 @@
 module DoneHtml exposing (..)
 
-import Html exposing (Html, button, p, text)
+import BEM exposing (bem, getElemClassFactory, getRootClass)
+import Html exposing (Html, button, p, section, text)
 
 import Html.Events exposing (onClick)
 import Play exposing (PlayMsg(..))
 import State exposing (Model)
 
+bemTools = bem "all-done"
+blockClass = getRootClass bemTools
+elemClass = getElemClassFactory bemTools
+
 doneView : Model -> Html PlayMsg
 doneView m =
-    p [] [ text "done ", button [ onClick  End ] [ text "finish"] ]
+    section [ blockClass ] [
+      p [ elemClass "info" ] [ text "All done!" ],
+      button [ onClick End, elemClass "action" ] [ text "OK" ]
+    ]
 
